497c16ac73234c15abd26010834ddd4cacdf5b0b
Bhanu Prasad Lade <bhanu.lade@valuemomentum.com>
2023-05-31 17:38:04 +0530

    created TestGitHubProject

---
+++ src/test/java/com/Test/github/githubSample.java
@@ -0,0 +1,11 @@
+package com.Test.github;
+
+public class githubSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("This is a GitHub Sample............");
+
+	}
+
+}
